2e86bac5be0fc77b3a27b437d45e8b3cd6b125ce
eloy <etoro@4geeks.com.ve>
2015-12-07 23:42:35 -0430

    exports.plugin => exports

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-/**
- * flatten
+/*
+ * var tools = require('mongoose-schema-tools')
+ * var MySchema = new Schema({...});
+ * MySchema.plugin(tools.plugin);
  *
- * @param {Object} val a js native object that resembles a mongoose model
+ * @param {Object} schema
  */
-var flatten = exports.flatten = function (val) {
+module.exports = function (schema) {
+    schema.methods.assign = function (source) {
+        var doc = this;
+        source = module.exports.flatten(source);
+        schema.eachPath(function (path, def) {
+            var isReadonly = !/^([^\.]\.)*_id$/.test(path) &&
+                !def.options.readonly;
+            if (isReadonly) {
+                doc.set(path, source[path]);
+            }
+        });
+    };
+};
+
+/**
+* flatten
+*
+* @param {Object} val a js native object that resembles a mongoose model
+*/
+var flatten = module.exports.flatten = function (val) {
     var acc = {};
     for (var key in val) {
-        var isSubset = (val[key] instanceof Object) &&
+        if (val[key] instanceof Object &&
             !('_id' in val[key]) &&
-            !(val[key] instanceof Array);
-        if (isSubset) {
+            !(val[key] instanceof Array)) {
             var subset = flatten(val[key]);
             for (var subkey in subset) {
                 acc[key + '.' + subkey] = subset[subkey];
@@ -19,25 +39,4 @@ var flatten = exports.flatten = function (val) {
         }
     }
     return acc;
-};
-
-/*
- * var tools = require('mongoose-schema-tools')
- * var MySchema = new Schema({...});
- * MySchema.plugin(tools.plugin);
- *
- * @param {Object} schema
- */
-module.exports.plugin = exports.plugin = function (schema) {
-    schema.methods.assign = function (source) {
-        var doc = this;
-        source = flatten(source);
-        schema.eachPath(function (path, def) {
-            var isReadonly = !/^([^\.]\.)*_id$/.test(path) &&
-                !def.options.readonly;
-            if (isReadonly) {
-                doc.set(path, source[path]);
-            }
-        });
-    };
 };
--- test/assign.js
+++ test/assign.js
@@ -10,6 +10,7 @@ describe('assigns a source object to a target doc', function () {
             prop2: String,
             prop3: Boolean,
             prop4: Schema.Types.ObjectId,
+            prop5: { type: String, readonly: true, default: 'no' },
             outerProp: {
                 innerProp: Number
             },
@@ -17,38 +18,64 @@ describe('assigns a source object to a target doc', function () {
                 nestedProp: Number
             }]
         });
-        testSchema.plugin(tools.plugin);
+        testSchema.plugin(tools);
         var testModel = mongoose.model('Test', testSchema);
 
-        var testDoc = new testModel({
-            arrOfProps: [{
-                nestedProp: 0
-            }, {
-                nestedProp: 0
-            }, {
-                nestedProp: 0
-            }]
+        var testChildSchema = new Schema({
+            prop6: String
         });
 
-        var boilerplate = {
-            prop1: 1,
-            prop3: true,
-            outerProp: {
-                innerProp: 3
-            }
-        };
+        testChildSchema.plugin(tools);
 
-        testDoc.assign({
+        var testChildModel = testModel.discriminator('TestChild', testChildSchema);
+
+        assertAssign(testModel, {
             _id: new Schema.Types.ObjectId,
             prop1: 1,
             prop3: true,
             outerProp: {
                 innerProp: 3
             }
+        }, {
+            prop1: 1,
+            prop3: true,
+            outerProp: {
+                innerProp: 3
+            }
         });
 
-        var testObj = testDoc.toObject();
+        assertAssign(testChildModel, {
+            prop3: true,
+            prop5: 'bye',
+            prop6: 'hello',
+            outerProp: {
+                innerProp: 3
+            }
+        }, {
+            prop3: true,
+            prop5: 'no',
+            prop6: 'hello',
+            outerProp: {
+                innerProp: 3
+            }
+        });
 
-        testObj.should.have.properties(boilerplate);
+        function assertAssign(model, actual, expected) {
+            var testDoc = new model({
+                arrOfProps: [{
+                    nestedProp: 0
+                }, {
+                    nestedProp: 0
+                }, {
+                    nestedProp: 0
+                }]
+            });
+
+            testDoc.assign(actual);
+
+            var testObj = testDoc.toObject();
+
+            testObj.should.have.properties(expected);
+        }
     });
 });
