703a4767eb2eeb1a73a4f7d37f4db492c4c18e63
Pacha <abraham@rsdev.com.ve>
2016-08-09 01:14:40 -0400

    ignore config

--- index.js
+++ index.js
@@ -8,12 +8,13 @@
 var _ = require('lodash');
 var PROTECTED_REGEXP = /^([^\.]*\.)*_/;
 
-module.exports = function (schema) {
+module.exports = function (schema, options) {
+    options = options || {};
     schema.methods.assign = function (source) {
         var doc = this;
         schema.eachPath(function (path, def) {
             var isProtected = PROTECTED_REGEXP.test(path);
-            if (!_.has(source, path) || def.options.readonly || isProtected)
+            if (!_.has(source, path) || def.options.readonly || isProtected || (options.ignore && options.ignore === path))
                 return;
             doc.set(path, _.get(source, path));
         });
